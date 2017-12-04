c2a6c2c09f7eb694e62085db6d15de9b96ff831f
Christopher Diep <diep.christopher@gmail.com>
2017-12-04 01:06:02 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,5 @@
 def reverse_each_word(sentence)
-  sentence_array = sentence.to_a
+  sentence_array = sentence.split(" ")
   reverse_sentence_array = []
   sentence_array.each do |word|
     reverse_sentence_array << word.reverse
